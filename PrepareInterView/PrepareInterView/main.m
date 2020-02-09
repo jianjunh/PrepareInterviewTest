6a1a647b3ff68233f93a7ff7156fe96d52836681
junjianh <935166120@qq.com>
2020-02-09 19:36:29 +0800

    创建项目

---
+++ PrepareInterView/PrepareInterView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PrepareInterView
+//
+//  Created by junjian_h@163.com on 2020/2/9.
+//  Copyright © 2020年 junjian_h@163.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
